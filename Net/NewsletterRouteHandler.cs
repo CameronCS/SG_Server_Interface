f6ee4d2b47e4b1a73a5936d5648ddf174464f774
CameronCS <camcstocks@gmail.com>
2024-10-15 22:16:42 +0200

    Added Get resources

---
+++ Net/NewsletterRouteHandler.cs
@@ -0,0 +1,20 @@
+﻿using SG_Server_Interface.Net.RawHandlers;
+using SG_Server_Interface.Responses.Newsletters.GetNewsletter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SG_Server_Interface.Net {
+    public class NewsletterRouteHandler(string API_URL, string Route) {
+        private readonly string API_URL = API_URL;
+        private readonly string Route = Route;
+
+        public async Task<GetNewslettersResponse> GetNewsletters() {
+            string url = $"{this.API_URL}{this.Route}";
+            GetNewslettersResponse @return = await NewsletterHandlerRaw.GetNewsletters(url);
+            return @return;
+        }
+    }
+}
---
+++ Net/RawHandlers/NewsletterHandlerRaw.cs
@@ -0,0 +1,29 @@
+﻿using SG_Server_Interface.Exceptions;
+using SG_Server_Interface.Responses.Newsletters.GetNewsletter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SG_Server_Interface.Net.RawHandlers {
+    public class NewsletterHandlerRaw {
+        private static HttpClient client = new();
+
+        public static async Task<GetNewslettersResponse> GetNewsletters(string url) {
+            HttpResponseMessage res = await client.GetAsync(url)
+            ??
+            throw new BadResponseExcpetion("Response Came back null please check urls");
+
+            GetNewsletterResponseRaw raw = await res.Content.ReadFromJsonAsync<GetNewsletterResponseRaw>()
+            ??
+            throw new BadResponseExcpetion("Response Came back null please check urls");
+
+            HttpStatusCode code = res.StatusCode;
+            GetNewslettersResponse @return = new(raw, (int)code);
+            return @return;
+        }
+    }
+}
